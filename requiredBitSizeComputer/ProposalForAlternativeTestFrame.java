9b8be7890d9e10545978932416a0169589b591c8
Michael Schaefers <Michael.Schaefers@HAW-Hamburg.de>
2017-10-07 17:53:34 +0200

    initial check-in

---
+++ requiredBitSizeComputer/ProposalForAlternativeTestFrame.java
@@ -0,0 +1,87 @@
+package requiredBitSizeComputer;
+
+
+//###
+//### Wenn Sie einen eigenen TestFrame entwickeln - was ausdruecklich gut waere
+//### dann sollten Sie den Klassennamen (mit "Refactoring") aendern
+//### Z.B. in  MyAlternativeTestFrame
+//###
+//### Ein Nachteil des gestellten TestFrames besteht darin, dass er interaktiv ist
+//### und damit fuer reproduzierbare Testlaeufe eigentlich NICHT geeignet ist.
+//### "Hier" sollten Sie Tests entwickeln, die leicht reproduzierbar sind,
+//### damit Sie im Falle eines gefundenen Fehlers auch (leicht) testen koennen, dass Sie
+//###   1.) den Fehler wirklich korrigiert und
+//###   2.) keine neuen Fehler bei bereits sicheren Code eingebaut
+//### haben.
+//###
+/**
+ * Your alternative TestFrame for Required Bit Size Computer
+ * 
+ * @author   Michael Schaefers ;  P1@Hamburg-UAS.eu 
+ * @version  2017/10/02
+ */
+public class ProposalForAlternativeTestFrame {
+    
+    public static void main( final String... unused ){
+        try{
+            final RequiredBitSizeComputer requiredBitSizeComputer = new RequiredBitSizeComputer();
+            
+            
+            
+            //VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
+            // Sofern Sie kein Vorwissen und/oder NICHT wissen was Sie tun
+            // fuehren Sie (bis auf die Aenderung des Klassennamens)
+            // KEINE Aenderungen oberhalb dieser Zeilen durch
+            //###
+            //###
+            //###
+            //###   HIER kommt Ihr Code zum Testen hin ;-)
+            //###
+            //###                    VVVV
+            //###                    VVVV
+            //###                    VVVV
+            //###   VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
+            //###      VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
+            //###         VVVVVVVVVVVVVVVVVVVVVVVVVV
+            //###            VVVVVVVVVVVVVVVVVVVV
+            //###               VVVVVVVVVVVVVV
+            //###                  VVVVVVVV
+            //###                     VV
+            
+            
+            
+            // Z.B. bzw. nur als erste Start-Idee/Anregung, wie so etwas aussehen koennte
+            long number;
+            int bitSize;
+            //
+            number = 100;
+            bitSize = requiredBitSizeComputer.computeBitSize( number );
+            System.out.printf( "Fuer die Zahl %d werden %d Bit(s) benoetigt.\n",  number, bitSize );
+            System.out.print( "\n\n" );
+            //
+            number = 200;
+            bitSize = requiredBitSizeComputer.computeBitSize( number );
+            System.out.printf( "Fuer die Zahl %d werden %d Bit(s) benoetigt.\n",  number, bitSize );
+            System.out.print( "\n\n" );         
+            
+            
+            //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+            // Sofern Sie kein Vorwissen haben und/oder NICHT wissen was Sie tun
+            // fuehren Sie KEINE Aenderungen unterhalb dieser Zeilen durch.
+            //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+            //
+            System.out.printf( "\n\n" );
+            System.out.printf( "THIS IS THE END" );
+        }catch( final Throwable ex ){
+            if( ! (( ex instanceof AssertionError ) || ( ex instanceof IllegalArgumentException ))){
+                System.out.flush();
+                System.err.printf( "###\n" );
+                System.err.printf( "### Uuuupppss, was ist denn da passiert ????\n" );
+                System.err.printf( "###\n" );
+                System.err.printf( "\n" );
+            }//if
+            throw( ex );
+        }//try
+    }//method()
+    
+}//class
---
+++ requiredBitSizeComputer/RequiredBitSizeComputer.java
@@ -0,0 +1,62 @@
+package requiredBitSizeComputer;
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * Wir erinnern uns: NIEMALS verschiedene Spachen in einem Programm mixen.
+ * Oder konkreter: Entweder Identifier und Kommentar in deutsch oder in englisch.
+ * Es gelten die in der Vorlesung besprochenen Regeln.
+ * Es lohnt sich immer, die Vorlesungsinhalte zu kennen ;-)
+ * 
+ * Auch ist diese Art von Kommentar KEIN guter Kommentar.
+ * Wir nutzen den Kommentar "hier" fuer Erklaerungen und Ausfuehrungen zur
+ * Aufgabenstellung. Es  gilt "hier" die Regel:
+ *  o) "Kommentar" der Kommentar ist in englisch.
+ *  o) "Kommentar" der die Aufgabenstellung vertieft in deutsch.
+ * Loeschen Sie derartigen Kommentar vor der Abgabe.
+ * 
+ * Im Rahmen der Aufgabenstellung muessen Sie GUTEN Kommentar schreiben.
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ */
+
+
+/**
+ * Required Bit Size Computer - see task
+ * 
+ * @author   Michael Schaefers ;  P1@Hamburg-UAS.eu
+ * @version  2017/10/07
+ */
+public class RequiredBitSizeComputer {
+    
+    /**
+     * compute required bit size to code positive number
+     * 
+     * @param number  the number for that the required bit size shall be computed
+     * @return required bit size to compute number
+     */
+    public int computeBitSize( final long number ){
+        //VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
+        //###
+        //###
+        //###
+        //###           HIER kommt Ihr Code hin
+        //###
+        //###                    VVVV
+        //###                    VVVV
+        //###                    VVVV
+        //###   VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
+        //###      VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
+        //###         VVVVVVVVVVVVVVVVVVVVVVVVVV
+        //###            VVVVVVVVVVVVVVVVVVVV
+        //###               VVVVVVVVVVVVVV
+        //###                  VVVVVVVV
+        //###                     VV
+        
+        int resu;
+        resu = 0;
+        
+        // berechne die Anzahl der benoetigten Bits um number zu codieren
+        
+        return resu;
+        
+        //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+    }//method()
+    
+}//class
